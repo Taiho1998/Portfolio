0f5d294e6f797556a1df8daa36f4b45d855acfc0
Taiho1998 <tgo12323@gmail.com>
2025-04-15 13:12:46 +0900

    Project 페이지 모바일에서 캐러셀 적용

--- src/app/(projects)/project/page.tsx
+++ src/app/(projects)/project/page.tsx
@@ -1,3 +1,4 @@
+import Carousel from "@/app/components/Carousel";
 import Navigate from "@/app/components/Navigate";
 import ProjDescription from "@/app/components/ProjectDescript";
 import { Metadata } from "next";
@@ -15,8 +16,11 @@ export default function projectPage() {
         <img
           src="/ProjectImg.svg"
           alt="baroFarm Introduce Img"
-          className="aspect-5/8 w-[150px] h-[240px] xl:w-[500px] xl:h-[800px] self-center xl:ml-[60px] mt-5 xl:mt-0"
+          className="hidden xl:block xl:aspect-5/8 w-[150px] h-[240px] xl: xl:w-[500px] xl:h-[800px] self-center xl:ml-[60px] mt-5 xl:mt-0"
         />
+        <div className="xl:hidden w-full mt-5">
+          <Carousel />
+        </div>
       </div>
     </div>
   );
---
+++ src/app/components/carousel.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { A11y, Autoplay, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+
+interface data {
+  img: string;
+  url: string;
+}
+
+export default function Carousel() {
+  const data: data[] = [
+    { img: "main", url: "000" },
+    { img: "login", url: "111" },
+    { img: "myPage", url: "222" },
+    { img: "category", url: "333" },
+    { img: "fruits", url: "444" },
+    { img: "barofarming", url: "555" },
+    { img: "basket", url: "666" },
+  ];
+
+  const swipeData = data.map((item, index) => (
+    <SwiperSlide key={index}>
+      <img
+        src={`/ProjectImg/Barofarm/${item.url}.png`}
+        alt={item.img}
+        className="h-[300px] object-cover mx-auto"
+      />
+    </SwiperSlide>
+  ));
+
+  return (
+    <section
+      style={{ height: `300px` }}
+      className="flex text-center mb-[10px] relative w-full"
+    >
+      <Swiper
+        style={{ height: "300px" }}
+        spaceBetween={50}
+        slidesPerView={1}
+        modules={[Pagination, A11y, Autoplay]}
+        pagination={{
+          clickable: true,
+          bulletActiveClass: "swiper-pagination-bullet-active",
+        }}
+        autoplay={{ delay: 3000, disableOnInteraction: false }}
+        loop={true}
+      >
+        {swipeData}
+      </Swiper>
+    </section>
+  );
+}
